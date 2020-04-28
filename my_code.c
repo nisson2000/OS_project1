6384f116bc63b0f99e5786078c7aaf2af2012bdb
nisson2000 <45344238+nisson2000@users.noreply.github.com>
2020-04-28 17:21:51 +0800

    Delete my_code.c

--- my_code.c
+++
@@ -1,9 +0,0 @@
-#include <linux/linkage.h>
-#include <linux/kernel.h>
-#include <linux/ktime.h>
-#include <linux/timekeeping.h>
-asmlinkage unsigned long long sys_my_getnstimeofday(void){
-    struct timespec now;
-    getnstimeofday(&now);
-    return (unsigned long long)now.tv_sec * 1000000000 + now.tv_nsec;
-}
